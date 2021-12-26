46f84785fb1349d3f8e2f077c57b871dd13c1b6a
Edison Nica <edisonn@0pii.com>
2021-12-26 00:04:05 -0500

    start prepare chestbuster

--- Facehugger/src/main/java/com/mercuryred/facehugger/Refactor.java
+++ Facehugger/src/main/java/com/mercuryred/facehugger/Refactor.java
@@ -4,7 +4,10 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.TypeDeclaration;
 
 import java.io.File;
@@ -16,6 +19,12 @@ import static com.github.javaparser.ast.Modifier.createModifierList;
 public class Refactor {
     static HashMap<String, TypeDeclaration> originalDepCUs = new HashMap<String, TypeDeclaration> ();
     static HashMap<String, TypeDeclaration> generatedDepInterfaces = new HashMap<String, TypeDeclaration> ();
+
+    static String chestbuster = "";
+
+    // todo factory imports
+    // todo Chestbuster.createFoo() -> constructors
+    // Facehugger .... generates what ...
 
     public static void main(String[] args) {
         try {
@@ -61,6 +70,53 @@ public class Refactor {
                         .filter(bodyDeclaration -> bodyDeclaration.isMethodDeclaration())
                         .toArray()[0]);
 
+        // todo ... port methods into teh interface
+
+
+
+        BodyDeclaration[] constructors =
+                cls.getPrimaryType()
+                .get()
+                .getMembers()
+                .stream()
+                .filter(bodyDeclaration -> bodyDeclaration.isConstructorDeclaration())
+                .toArray(BodyDeclaration[]::new);
+
+        System.out.println(constructors[0]);
+
+
+        if (constructors.length == 0) {
+        // This should be based on constructors
+        chestbuster = chestbuster +
+                "\n" +
+                "public static " + clsName + " create" + clsName + "() {\n" +
+                "  return null;\n" +
+                "}\n";
+        } else {
+            for (BodyDeclaration cnstr: constructors) {
+                String code = "\n" +
+                        "public static " + clsName + " create" + clsName + "(";
+
+                NodeList<Parameter> params = cnstr.asConstructorDeclaration().getParameters();
+
+                for (int i = 0 ; i < params.size(); i++) {
+                    code = code + params.get(i).getTypeAsString() + " " + params.get(i).getNameAsString();
+                    if (i < params.size() - 1) {
+                        code = code + ", ";
+                   }
+                }
+
+                code = code + ") {\n";
+                code = code +
+                        "  return null;\n" +
+                        "}\n";
+
+                chestbuster = chestbuster + code;
+            }
+        }
+
         System.out.println(newInterface);
+
+        System.out.println(chestbuster);
     }
 }
